eae787418b6bb83d3c702670cd26a94f80748685
Belal Elsabbagh <belsabbagh@gmail.com>
2025-12-18 23:40:32 +0200

    fixed endpoint

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,7 +21,7 @@ async function handleSubmit(event) {
 	data.numMeters = parseInt(data.numMeters, 10);
 
 	try {
-		const response = await fetch("http://localhost:5515/run", {
+		const response = await fetch("/api/run", {
 			method: "POST",
 			headers: { "Content-Type": "application/json" },
 			body: JSON.stringify(data),
